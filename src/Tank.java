09fc72ec2b513beaf66f560e93cd12b1c8d5141f
Sarkis <sarkis.mkertchyan.00@gmail.com>
2023-04-23 20:04:07 +0300

    First commit

--- src/Tank.java
+++ src/Tank.java
@@ -1,18 +1,53 @@
 public class Tank {
-    public int position;
+    int x,y;
+    int dir;
+    int fuel = 100;
 
-    public void goForward(int pos){
-        this.position += pos;
-    }
-    public void newFinc(){
-        
-    }
-    public void goBackward(int pos){
-        this.position -= pos;
+    public Tank(){
+
     }
 
-    public void printPosition(){
-        System.out.println("The Tank is at " + position + " now.");
+    public Tank(int x, int y){
+        this.x = x;
+        this.y = y;
+    }
+
+    public Tank(int x, int y, int fuel){
+        this.x = x;
+        this.y = y;
+        this.fuel = fuel;
+    }
+
+    public void goForward(int i) {
+        if (fuel >= i){
+            fuel -= i;
+        }else {
+            i = fuel;
+        }
+        if (dir == 0) x += i;
+        else if (dir == 1) y += i;
+        else if (dir == 2) x -= i;
+        else y -= i;
+
+    }
+    public void printPosition() {
+        System.out.println("The Tank is at " + x + ", " + y + " now.");
+    }
+
+    public void turnLeft() {
+        dir--;
+        if (dir == -1) dir = 3;
+
+    }
+
+    public void turnRight() {
+        dir++;
+        if (dir == -1) dir = 3;
+
+    }
+
+    public void goBackward(int i) {
+        goForward(-i);
     }
 }
 
